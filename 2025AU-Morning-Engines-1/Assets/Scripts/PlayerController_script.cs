b7a4c90eb30cabd937b7de632215351c34127eac
Gavin-197 <gcorder1@student.cscc.edu>
2025-11-08 23:41:18 -0500

    Movement & Price values added

--- 2025AU-Morning-Engines-1/Assets/Scripts/PlayerController_script.cs
+++ 2025AU-Morning-Engines-1/Assets/Scripts/PlayerController_script.cs
@@ -1,15 +1,16 @@
 using UnityEngine;
 
-
 /* Description:
-    -Will take all input from the player that isn’t through any UI elements like the keypad
+    +Will take all input from the player that isn’t through any UI elements like the keypad
         -Pausing, scrolling, etc.
-    -Will move the camera when scrolling with the mouse (if the camera will be that close to the vending machine)
+    +Will move the camera when scrolling with the mouse (if the camera will be that close to the vending machine)
 
 */
 public class PlayerController_script : MonoBehaviour
 {
     public float playerMoney;
+
+    private float scrollSpeed = 0.05f;
     void Start()
     {
         
@@ -17,6 +18,15 @@ public class PlayerController_script : MonoBehaviour
 
     void Update()
     {
-        
+        if ((Input.mouseScrollDelta.y > 0) && (transform.position.y < 0.7f))
+        {
+            transform.Translate(Vector3.up * scrollSpeed);
+        }
+        else if ((Input.mouseScrollDelta.y < 0) && (transform.position.y > -0.5f))
+        {
+            transform.Translate(Vector3.down * scrollSpeed);
+        }
+
+
     }
 }
--- 2025AU-Morning-Engines-1/Assets/Scripts/VendingMachine_script.cs
+++ 2025AU-Morning-Engines-1/Assets/Scripts/VendingMachine_script.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 /* Description:
     Depends on: 
-        -GameController_script for starting
+        +GameController_script for starting
         -SnackController_script for organization of snacks
         -Keypad_script for selection of snacks
     This script will be mostly used to cycle through every snack object for varying reasons like:
@@ -13,15 +13,28 @@ using UnityEngine;
  */
 public class VendingMachine_script : MonoBehaviour
 {
-    private GameObject[] snacks; // Array with all of the snacks in it
+    [SerializeField] private GameObject[] snacks; // Array with all of the snacks in it
     private int input; // input from the keypad UI
+    private string[] positionIDs = {"A1", "A2", "A3", "A4", "B1", "B2", "B3", "B4", "C1", "C2", "C3", "C4"};
+    private Vector3[] positionVectors = { 
+        new Vector3(0, 0, 0), new Vector3(1, 1, 0), new Vector3(2, 1, 0), new Vector3(3, 1, 0), 
+        new Vector3(0, 2, 0), new Vector3(1, 2, 0), new Vector3(2, 2, 0), new Vector3(3, 2, 0),
+        new Vector3(0, 3, 0), new Vector3(1, 3, 0), new Vector3(2, 3, 0), new Vector3(3, 3, 0),
+        new Vector3(0, 4, 0), new Vector3(1, 4, 0), new Vector3(2, 4, 0), new Vector3(3, 4, 0)}; // Possible positions for snacks
 
-    void Start()
+
+    private void Start()
     {
-        
+        // for each snack, put them in their spot, and assign them their positionID (A1, A2, etc.)
+        for (int i = 0; i < 4 /*snacks.Length*/; i++)
+        {
+
+            GameObject indexedSnack = Instantiate(snacks[i], positionVectors[i] + transform.position, Quaternion.identity, transform);
+        }
+
     }
 
-    void Update()
+    private void Update()
     {
         
     }
