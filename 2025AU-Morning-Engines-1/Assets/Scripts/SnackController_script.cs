9391a8b889d6ecc354ef1981b7742d4f501ad4f6
PY-Student-Acc <pyoungkin@student.cscc.edu>
2025-11-12 09:00:55 -0500

    SFX
    added sfx files, a txt file with creative commons attributions, and added comments to a script

--- 2025AU-Morning-Engines-1/Assets/Scripts/SnackController_script.cs
+++ 2025AU-Morning-Engines-1/Assets/Scripts/SnackController_script.cs
@@ -11,6 +11,12 @@ public class SnackController_script : MonoBehaviour
     public float snackCost;   // Cost of the snack
     public int snackStatus;   // Whether or not the snack has been emptied from the machine or not (0 is the starting value, 1 is when the snack is stuck, and 2 is when the snack has been dropped)
     public bool willGetStuck; // Determines if the snack will get stuck or not
+    // From Pin, change to an int value. 
+    // 0 is that it won't be 
+    // 1 is that it needs to be bought twice because it's a row back
+    // 2 is it will get stuck
+    // then we could use this variable for the visual part maybe
+    // also maybe add a thing were Ints 3 and over
     public string positionID = null;
     void Start()
     {
