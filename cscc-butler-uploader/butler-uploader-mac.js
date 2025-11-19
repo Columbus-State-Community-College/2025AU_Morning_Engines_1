17286e1fed3fba5ae69082b2e8c018191fbdab90
MVG Tom <tallenbaugh@cscc.edu>
2025-11-19 11:32:21 -0500

    cscc-butler-uploader version fix for mac

--- cscc-butler-uploader/butler-uploader-mac.js
+++ cscc-butler-uploader/butler-uploader-mac.js
@@ -23,7 +23,7 @@ const username = "cscc-mini-projects";
 // ESPECIALLY IF YOU CHANGE THE GAME URL ON itch.io
 const gamename = "2025au-m1-stuck-snacks";
 // If you're on Linux, you may need to change this...
-const butler_command_file = "./butler/versions/15.21.0/mac/butler";
+const butler_command_file = "./butler/versions/15.24.0/mac/butler";
 // END OF ATTENTION BLOCK
 
 function CurrentVersionString() {
