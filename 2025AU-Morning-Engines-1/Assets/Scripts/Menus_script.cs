36d8af0368f5e59bfba2f4706c88ece566a5228f
Gavin-197 <gcorder1@student.cscc.edu>
2025-11-24 19:52:19 -0500

    Controller integrated UI
    The keypad and the main menus will now work on controller. Also added helpful selection colors for the menus.

--- 2025AU-Morning-Engines-1/Assets/Scripts/Menus_script.cs
+++ 2025AU-Morning-Engines-1/Assets/Scripts/Menus_script.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 /* Description:
  * Used for storing methods that may be useful for the main menu
@@ -7,6 +8,18 @@ using UnityEngine.EventSystems;
 
 public class Menus_script : MonoBehaviour
 {
+    [SerializeField] private Button startButton;
+
+    public void Start()
+    {
+        startButton.Select();
+    }
+
+    public void SelectButton(Button button)
+    {
+        button.Select();
+    }
+
     public void exitgame()
     {
         Debug.Log("NOTE: Exit game only works during a build, not in the editor!");
