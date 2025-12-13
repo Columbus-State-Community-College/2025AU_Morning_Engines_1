fd48721538960c73135953491b47a035375bcab1
PY-Student-Acc <pyoungkin@student.cscc.edu>
2025-12-13 13:30:38 -0500

    Contribution

--- 2025AU-Morning-Engines-1/Assets/Scripts/PlayerController_script.cs
+++ 2025AU-Morning-Engines-1/Assets/Scripts/PlayerController_script.cs
@@ -3,7 +3,7 @@ using TMPro;
 using UnityEngine;
 
 /* Description:
-    +Will take all input from the player that isn’t through any UI elements like the keypad
+    +Will take all input from the player that isnï¿½t through any UI elements like the keypad
         +Pausing, scrolling, etc.
     +Will move the camera when scrolling with the mouse (if the camera will be that close to the vending machine)
 
@@ -46,11 +46,28 @@ public class PlayerController_script : MonoBehaviour
     private void ChangePlayerMoney(SnackController_script snackController)
     {
         playerMoney -= snackController.snackCost;
+        //Pay_Animation();
         cashTextElement.text = playerMoney.ToString();
     }
 
     private void OnDisable()
     {
         SnackController_script.OnSnackBought -= ChangePlayerMoney;
+    }
+
+    public void Pay_Animation(SnackController_script snackController)
+    {
+        float CurrentCost = snackController.snackCost; 
+        if (CurrentCost == 0.99)
+        {
+         //   GetComponent<Animation>().play("billinsert_animation");
+        }
+        // 0.99 play coin animation 9 times
+
+
+        // 1.49 play bill animation 1 time and coin animation 7 times
+        // 1.99 play bill animation 1 time and coin animation 9 times
+        // 2.49 play bill animation 2 times and coin animation 7 times
+        // 2.99 play bill animation 2 times and coin animation 9 times
     }
 }
